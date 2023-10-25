35a918c307af5d6242948692e26de29c549c43b9
teabookcodes <simon.kana@seznam.cz>
2023-10-25 02:59:43 +0200

    Disabled CrossOrigin

--- src/main/java/com/simonkana/bookmanapi/controller/BookController.java
+++ src/main/java/com/simonkana/bookmanapi/controller/BookController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "https://bookman-lms.vercel.app"})
 @RestController
 @RequestMapping("/api")
 public class BookController {
